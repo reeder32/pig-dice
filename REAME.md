8a800e89743ec8859abd851095ed094b5d6382ff
Nicholas Reeder <nickreeder32@gmail.com>
2021-04-07 14:37:29 -0700

    add tests for pass function
    co-authored-by: Michael Burton <micbur1@gmail.com>
    Co-authored-by: Sammai Gutierrez <sammaigtz@hotmail.com>

--- REAME.md
+++ REAME.md
@@ -1,3 +1,4 @@
+## Player Tests
 Describe: addScore()
 
 Test: "It should add the value of the current role to the player's score"
@@ -9,8 +10,25 @@ Code: player.addRoll(5)
 Expected Output: 5
 
 
-Describe roll()
+Describe rollDice()
 
 Test: "It should return a number a number between 1 and 6"
 Code: roll()
 Expected Output: 1,2,3,4,5,6
+
+## Game Tests
+Describe: addPlayer()
+Test: "It should add the player object to the players object of the Game"
+Code: let game = new Game();
+      game.addPlayer( new Player("Nick"));
+Expected Output: [ Player { name: 'Nick', currentRoll: 0, score: 0 } ]
+
+Describe: pass()
+Test: "It should assign the player that is passed in the arguement to the matching index and assign the next player as the currentPlayer of the game"
+Code: let game = new Game();
+let nick = new Player("Nick")
+let sammai = new Player("Sammai")
+      game.addPlayer(nick);
+      game.addPlayer(sammai);
+      game.pass(nick);
+Expected Output: Game {players: [Player{name: "Nick", currentRoll: 0, score: 0},Player{name: "Sammai", currentRoll: 0, score: 0} ], currentPlayer: Player{name: "Sammai", currentRoll: 0, score: 0}}
--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,40 @@
+// Utility logic
+function roll() {
+  return Math.floor(Math.random() * 6) + 1;
+}
 // Business logic
-function Player(name, currentRoll, score) {
+
+// Game logic
+function Game() {
+  this.players = [];
+  this.currentPlayer = undefined;
+}
+
+
+Game.prototype.addPlayer = function (player) {
+  this.players.push(player);
+}
+
+Game.prototype.pass = function (player) {
+  for (let i = 0; i < this.players.length; i++) {
+    const p = this.players[i];
+    //console.log(p.name);
+    if (p.name === player.name) {
+      this.players[i] = player;
+      if (i < this.players.length - 1) {
+        game.currentPlayer = this.players[i + 1]
+      } else {
+        game.currentPlayer = this.players[0]
+      }
+    }
+  }
+}
+
+// Player logic
+function Player(name) {
   this.name = name
-  this.currentRoll = currentRoll
-  this.score = score
+  this.currentRoll = 0
+  this.score = 0
 }
 
 Player.prototype.addScore = function () {
@@ -10,14 +42,26 @@ Player.prototype.addScore = function () {
   this.currentRoll = 0;
 }
 
-Player.prototype.addRoll = function (rollValue) {
-  this.currentRoll += rollValue;
+Player.prototype.rollDice = function () {
+  const rollValue = roll();
+  if (rollValue === 1) {
+    this.currentRoll = 0
+
+  } else {
+    this.currentRoll += rollValue;
+  }
 }
 
-function roll() {
-  return Math.floor(Math.random() * 6) + 1;
-}
 
-let player = new Player("Nick", 0, 0);
-console.log(roll());
+
+//let player = new Player("Nick", 0, 0);
+let game = new Game();
+let nick = new Player("Nick");
+game.addPlayer(nick);
+let sammai = new Player("Sammai");
+game.addPlayer(sammai);
+let michael = new Player("Michael");
+game.addPlayer(michael);
+game.pass(michael);
+console.log(game.currentPlayer);
 // Ui logic
